8920a4c3786bbda292387e327a3ed03fe63b38cb
tesiawu <tesiawu@utexas.edu>
2017-09-25 19:45:30 -0500

    Changed to characters and people names

--- nav.py
+++ nav.py
@@ -9,10 +9,10 @@ nav = Nav()
 nav.register_element('top', Navbar('',
     View('Home', 'frontend.show', page='index.html'),
     View('About', 'frontend.show', page='about.html'),
-    View('Superheroes', 'frontend.show', page='superheros.html'),
+    View('Characters', 'frontend.show', page='characters.html'),
     View('Movies', 'frontend.show', page='movies.html'),
     View('TV Shows', 'frontend.show', page='tvshows.html'),
     View('Comics', 'frontend.show', page='comics.html'),
-    View('Creators', 'frontend.show', page='creators.html'),
+    View('People', 'frontend.show', page='people.html'),
     View('Events', 'frontend.show', page='events.html'),
     ))
