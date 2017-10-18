fc75a919d5f340f482de6422e449ffe620bb7f74
Andrew Martin <andrewcm@utexas.edu>
2017-10-17 19:44:20 -0500

    Added src folder with react code

--- app/models.py
+++ app/models.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import flask.ext.restless
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
