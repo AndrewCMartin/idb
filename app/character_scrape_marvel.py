bc2959e442f74359990ed32b4fac06e23b4250fe
tesiawu <tesiawu@utexas.edu>
2017-10-19 16:49:23 -0500

    Create character_scrape_marvel.py

---
+++ app/character_scrape_marvel.py
@@ -0,0 +1,72 @@
+import hashlib, requests
+import time, json
+
+base_url = 'https://gateway.marvel.com/v1/public/'
+k_priv = 'fdf9c8bc5c83cbe565fdd6ddc4df9d0fb1e38a83'
+k_pub = '7f39855a0661b5fe55f842d7afa8cd9f'
+
+def compute_hash():
+    m = hashlib.md5()
+    ts = str(time.time())
+    m.update(ts.encode('utf-8'))
+    m.update(k_priv.encode('utf-8'))
+    m.update(k_pub.encode('utf-8'))
+    h = m.hexdigest()
+    return (ts, h)
+ 
+
+def marvel_get(endpoint, params=None):
+    ts, h = compute_hash()
+    data = {'ts':ts, 'hash': h, 'apikey': k_pub}
+    if params:
+        data.update(params)
+    return requests.get(base_url+endpoint, params=data)
+
+
+for offset in range(0, 2000, 20):
+
+    r = marvel_get('characters', {'offset':str(offset)})
+
+    d = json.loads(r.text)
+    d
+
+    for char_keys, char_data in d['data'].items():
+        if char_keys == 'results':
+            for char in char_data:
+                for char_attr_keys, char_attr in char.items():
+                    if char_attr_keys == 'name':
+                        name = str(char_attr)
+                        print('Name: ' + name)
+                    elif char_attr_keys == 'id':
+                        id_name = str(char_attr)
+                        print('ID: ' + id_name)
+                    elif char_attr_keys == 'thumbnail':
+                        path = str(char_attr['path'])
+                        for v in path.split('/'):
+                            if v == 'image_not_available':
+                                path = None
+                                
+                        if path != None:
+                            path = path + '.' + char_attr['extension']
+                    elif char_attr_keys == 'description':
+                        desc = str(char_attr)
+                        print('Description: ' + desc)
+                    elif char_attr_keys == 'stories':
+                        # Stories have their own dict to go through
+                        print('Stories: ')
+                        for story in char_attr['items']:
+                            story_name = str(story['name'])
+                            print(story_name)
+                    elif char_attr_keys == 'events':
+                        # Events have their own dict to go through
+                        print('Events: ')
+                        for event in char_attr['items']:
+                            event_name = str(event['name'])
+                            print(event_name)
+                    elif char_attr_keys == 'series':
+                        # Series have their own dict to go through
+                        print('Series: ')
+                        for series in char_attr['items']:
+                            series_name = str(series['name'])
+                            print(series_name)
+                    print('\n')
