d0a82673e8555d92348b67dded6bc4c13f805a3e
Sunaina Krishnamoorthy <sunaina.krishnamoorthy@gmail.com>
2017-10-22 18:56:54 -0500

    added tv show scraper thingy

---
+++ app/tvshow_scrape_tmdb.py
@@ -0,0 +1,48 @@
+import hashlib, requests
+import time, json
+from models import db, TvShow
+
+import tmdbsimple as tmdb
+tmdb.API_KEY = 'ad40b463da9a53ce7faffa8cc87d4d6a'
+
+def tvshow_info(tvshow_id):
+    tvshow = tmdb.TV(tvshow_id)
+    response = tvshow.info()
+
+    credits = tvshow.credits()
+
+    characters = []
+    actors = []
+
+    for person in credits["cast"]:
+        characters.append(person["character"])
+        actors.append(person["name"])
+
+    print (tvshow_id, tvshow.name)
+
+    #Create the character with the schema from models.py
+    newEntry = TvShow(tvshow_id, tvshow.name, tvshow.overview, tvshow.poster_path, tvshow.runtime, tvshow.last_air_date, tvshow.languages, tvshow.vote_average, tvshow.number_of_seasons, tvshow.number_of_episodes, str(characters), str(actors))
+    db.session.merge(newEntry)
+    db.session.commit()
+
+def marvel_shows(page_num):
+    params['page'] = page_num
+
+    tvshows = requests.get(base_url, params)
+
+    for tvshow in tvshows["results"]:
+        tvshow_info(tvshow["id"])
+
+
+# There are only 44 results total, the results are in 3 pages
+base_url = 'https://api.themoviedb.org/3/search/tv'
+params = { 'api_key' : 'ad40b463da9a53ce7faffa8cc87d4d6a', 
+            'language' : 'en-US',
+            'query' : 'marvel',
+            'page' : 0
+         }
+
+for i in range(1, 4):
+    marvel_shows(i)
+
+print('Done')
