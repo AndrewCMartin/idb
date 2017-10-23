7b027a69112287e01302c727367aca8f205f4b03
Bhavish Yalamanchi <bhavish@cs.utexas.edu>
2017-10-23 18:36:21 -0500

    fix

--- src/Movies.js
+++ src/Movies.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { axios } from 'axios'
+
+function getMovies(){
+  return axios.get('http://marvelus.me/api/movie?q=%7B%7D');
+}
+
+
+var movies = getMovies();
+movies.then(function(data) {
+
+});
 
 var moviesInGrid = [];
-for (var i = 0; i < numChars; i++) {
+
+for (var i = 0; i < movies.objects.length; i++) {
     moviesInGrid.push(
         <div class="col-sm-4">
             <div class="panel panel-info">
-                <div class="panel-heading"><Link to="{movies[i].getLink()}">{movies[i].getName()}</Link></div>
-                <div class="panel-body"><img src="{movies[i].getImageLink()}" class="img-responsive" style="width:100%" alt="Image" /></div>
-                <div class="panel-footer">{movies[i].getName()}</div>
+                <div class="panel-heading"><Link to="{movies[i].getLink()}">{movies.objects[i].getName()}</Link></div>
+                <div class="panel-body"><img src="{movies.objects[i].getImageLink()}" class="img-responsive" style="width:100%" alt="Image" /></div>
+                <div class="panel-footer">{movies.objects[i].getName()}</div>
             </div>
         </div>
     );
