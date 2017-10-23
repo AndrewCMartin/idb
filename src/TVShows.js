84a83d9f75a76c4433a2d86fd9d489bd165f2ad0
Hannah Anees <hannahan@cs.utexas.edu>
2017-10-23 18:07:53 -0500

    Added UML diagram

--- src/TVShows.js
+++ src/TVShows.js
@@ -1,38 +1,113 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const TVShows = () => (
-    <div class="container" style="margin-top:100px;">
-          <div class="row">
-            <div class="col-sm-4">
-              <div class="panel panel-info">
-                <div class="panel-heading"><a href="{{url_for('frontend.show', page='agentsofshield.html')}}">Agents of S.H.I.E.L.D.</a></div>
-                <div class="panel-body"><img src="https://image.tmdb.org/t/p/w500/3CFeptFvp0CE6H665Fpzq8JHxWX.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-                <div class="panel-footer">
-                  Agents of S.H.I.E.L.D.
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="panel panel-success">
-                <div class="panel-heading"><a href="{{url_for('frontend.show', page='agentcarter.html')}}">Agent Carter</a></div>
-                <div class="panel-body"><img src="https://image.tmdb.org/t/p/w500/mQWBExOnHrGVsjkpWczJjo6bhZy.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-                <div class="panel-footer">
-                    Agent Carter
-                  </div>
-              </div>
-            </div>
-            <div class="col-sm-4">
-              <div class="panel panel-danger">
-                <div class="panel-heading"><a href="{{url_for('frontend.show', page='defenders.html')}}">Defenders</a></div>
-                <div class="panel-body"><img src="https://image.tmdb.org/t/p/w500/49XzINhH4LFsgz7cx6TOPcHUJUL.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
-                <div class="panel-footer">
-                    Defenders
-                  </div>
-              </div>
-            </div>
-          </div>
-        </div><br>
-)
+// Just trying this out
+function TVGrid(props) {
+  return(
+    <ul className="data-list">
+      {props.tvshows.map(function (tvshow) {
+        return (
+          <li key={tvshow.tvshowID} className='data-item'>
+            <ul className='data-list-items'>
+                <Link to={'/TVShowInstance/' + tvshow.tvshowID}>
+                  <li>
+                    <img
+                      className='img'
+                      src={tvshow.Image}
+                      alt={'Image for ' + tvshow.Title}/>
+                  </li>
+                  <li>{tvshow.Title}</li>
+                </Link>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+TVGrid.propTypes = {
+  tvshows : PropTypes.array.isRequired,
+};
+
+class TVShows extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentFilter: "Show All",
+      tvshows: null,
+    };
+
+    this.updateFilter = this.updateFilter.bind(this);
+  }
+  componentDidMount() {
+    this.updateFilter(this.state.currentFilter)
+  }
+  updateFilter(filter) {
+    this.setState(function() {
+      return {
+        currentFilter: filter,
+        tvshows: null,
+      }
+    });
+    api.getTVShows("Show All")
+      .then(function(tvshows) {
+        this.setState(function() {
+          return {
+            tvshows: tvshows
+          }
+        })
+      }.bind(this))
+  }
+
+  render() {
+    const {tvshow} = this.state;
+    return (
+      <div>
+        {!this.state.tvshows
+          ? <p>LOADING</p>
+          : <AlbumGrid albums={this.state.tvshows} />}
+
+      </div>
+    )
+  }
+}
 
 export default TVShows
+
+
+// const TVShows = () => (
+//     <div class="container" style="margin-top:100px;">
+//           <div class="row">
+//             <div class="col-sm-4">
+//               <div class="panel panel-info">
+//                 <div class="panel-heading"><a href="{{url_for('frontend.show', page='agentsofshield.html')}}">Agents of S.H.I.E.L.D.</a></div>
+//                 <div class="panel-body"><img src="https://image.tmdb.org/t/p/w500/3CFeptFvp0CE6H665Fpzq8JHxWX.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+//                 <div class="panel-footer">
+//                   Agents of S.H.I.E.L.D.
+//                 </div>
+//               </div>
+//             </div>
+//             <div class="col-sm-4">
+//               <div class="panel panel-success">
+//                 <div class="panel-heading"><a href="{{url_for('frontend.show', page='agentcarter.html')}}">Agent Carter</a></div>
+//                 <div class="panel-body"><img src="https://image.tmdb.org/t/p/w500/mQWBExOnHrGVsjkpWczJjo6bhZy.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+//                 <div class="panel-footer">
+//                     Agent Carter
+//                   </div>
+//               </div>
+//             </div>
+//             <div class="col-sm-4">
+//               <div class="panel panel-danger">
+//                 <div class="panel-heading"><a href="{{url_for('frontend.show', page='defenders.html')}}">Defenders</a></div>
+//                 <div class="panel-body"><img src="https://image.tmdb.org/t/p/w500/49XzINhH4LFsgz7cx6TOPcHUJUL.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+//                 <div class="panel-footer">
+//                     Defenders
+//                   </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div><br>
+// )
+
+// export default TVShows
