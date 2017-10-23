70d622f4b4da6bf2108ef22bd1e7d16e05c23b7e
Bhavish Yalamanchi <bhavish@cs.utexas.edu>
2017-10-23 17:45:50 -0500

    react grid and instance pages for characters and movies

--- src/CharacterInstance.js
+++ src/CharacterInstance.js
@@ -1,8 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const CharacterInstance = () => (
+var relevantMovies = [];
+for(var i = 0; i < character.relevantMovies.length; i++) {
+    relevantMovies.push(<li><Link to="{character.relevantMovies[i].getLink()}">character.relevantMovies[i].name</Link></li>);
+}
 
+var comicSeries = [];
+for(var i = 0; i < character.comicSeries.length; i++) {
+    comicSeries.push(<li><Link to="{character.comicSeries[i].getLink()}">character.comicSeries[i].name</Link></li>);
+}
+
+const CharacterInstance = () => (
+<div class="container" style="margin: auto; margin-top:100px; width: 500px">
+    <div class="panel panel-default" >
+        <div class="panel-heading">
+            <h1>{character.name}</h1>
+        </div>
+        <div class="panel-body">
+            <img src="{movie.thumbnail}" class="img-responsive" style="width:100%" alt="Image" />
+            <h3>Information</h3>
+            <ul>
+                <li><b>Name:</b> {character.name}</li>
+                <li><b>Power:</b> {character.power}</li>
+                <li><b>Origin:</b> {character.desc}</li>
+                <li><b>Real Name:</b> {character.real_name}</li>
+                <li>
+                    <b>Relevant Movies/TV Shows:</b>
+                    <ul> 
+                        {relevantMovies}
+                    </ul>
+                </li>
+                <li>
+                    <b>Comic Series:</b>
+                    <ul> 
+                        {comicSeries}
+                    </ul> 
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
 )
 
 export default CharacterInstance
--- src/MovieInstance.js
+++ src/MovieInstance.js
@@ -1,8 +1,47 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const MovieInstance = () => (
+var movieChars = [];
+for(var i = 0; i < movie.characters.length; i++) {
+    movieChars.push(<li><Link to="{movie.characters[i].getLink()}">movie.characters[i].name</Link></li>);
+}
 
+var movieCast = [];
+for(var i = 0; i < movie.cast.length; i++) {
+    movieCast.push(<li><Link to="{movie.cast[i].getLink()}">movie.cast[i].name</Link></li>);
+}
+
+const MovieInstance = () => (
+<div class="container" style="margin: auto; margin-top:100px; width: 500px">
+    <div class="panel panel-default" >
+        <div class="panel-heading">
+            <h1>{movie.title}</h1>
+        </div>
+        <div class="panel-body">
+            <img src="{movie.poster_path}" class="img-responsive" style="width:100%" alt="Image" />
+            <h3>Information</h3>
+            <ul>
+                <li><b>Year:</b> {movie.release_date}</li>
+                <li><b>Director:</b> {movie.director}</li>
+                <li><b>Writer:</b> {movie.writers}</li>
+                <li>
+                    <b>Main Characters:</b>
+                    <ul>
+                        {movieChars}
+                    </ul>
+                </li>
+                <li>
+                    <b>Cast:</b> 
+                    <ul>
+                        {movieCast}
+                    </ul>
+                </li>
+                <li><b>Runtime:</b> {movie.runtime}</li>
+                <li><b>Rating:</b> {movie.rating}</li>
+            </ul>
+        </div>
+    </div>
+</div>
 )
 
 export default MovieInstance
