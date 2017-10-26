1194273bd70e499ee3b348a6d59aee32f0e19c78
Hannah Anees <hannahanees@utexas.edu>
2017-10-26 01:20:43 +0000

    Fixed formatting on Character and Movie grid pages

--- src/Characters.js
+++ src/Characters.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 var axios = require('axios');
 
+var imageStyles = {
+    width: '500px',
+    height: '400px'
+}
 class Characters extends React.Component {
     constructor(props) {
         super(props);
@@ -12,7 +16,7 @@ class Characters extends React.Component {
     }
     
     componentDidMount() {
-        return axios.get('http://marvelus.me/api/character?q=%7B%22limit%22:9%7D').then(res=> {
+        return axios.get('http://marvelus.me/api/character?q=%7B%22filters%22%3A[%7B%22name%22%3A%22thumbnail%22%2C%22op%22%3A%22is_not_null%22%7D]%7D').then(res=> {
             const characters = res.data.objects.map(character => character)
             this.setState({characters});
         });
@@ -26,8 +30,7 @@ class Characters extends React.Component {
                 <div className="col-sm-4">
                   <div className="panel panel-info">
                     <div className="panel-heading"><Link to={"/character/" + character.id}>{character.name}</Link></div>
-                    <div className="panel-body"><img src={character.thumbnail} className="img-responsive" styles="width:100%" alt="Image" /></div>
-                    <div className="panel-footer">{character.name}'s stories: {character.stories}</div>
+					 <div className="panel-body"><img src={character.thumbnail} className="img-responsive" style={imageStyles} alt="Image" /></div>
                   </div>
                 </div>
               )}
--- src/Movies.js
+++ src/Movies.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 var axios = require('axios');
 
+var imageStyles = {
+    width:'400px',
+    height: '450px'
+}
+
+
 class Movies extends React.Component {
   constructor(props) {
     super(props);
@@ -26,7 +32,7 @@ class Movies extends React.Component {
             <div className="col-sm-4">
               <div className="panel panel-info">
                 <div className="panel-heading"><Link to={"/movie/" + movie.id}>{movie.title}</Link></div>
-                <div className="panel-body"><img src={"http://image.tmdb.org/t/p/w185/" + movie.poster_path} className="img-responsive" styles="width:100%" alt="Image" /></div>
+				 <div className="panel-body"><img src={"http://image.tmdb.org/t/p/w500/" + movie.poster_path} className="img-responsive" style={imageStyles} alt="Image" /></div>
               </div>
             </div>
           )}
