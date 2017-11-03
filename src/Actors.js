7be9f1d7e84d23020a7298609c3ebc2648bf98af
Hannah Anees <hannahanees@utexas.edu>
2017-11-03 01:13:30 +0000

    updated sorting and reformatted grid

--- src/Actors.js
+++ src/Actors.js
@@ -8,12 +8,24 @@ var imageStyles = {
     height: '500px',
 }
 
+function splitarray(input, spacing)
+{
+    var output = [];
+
+    for (var i = 0; i < input.length; i += spacing)
+    {
+        output[output.length] = input.slice(i, i + spacing);
+    }
+
+    return output;
+}
 
 class Actors extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             actors: [],
+            actorsGrouped: [],
             numPages: 1,
             activePage: 1,
             resultsPerPage: 6,
@@ -43,7 +55,9 @@ class Actors extends React.Component {
             this.state.numPages = res.data.total_pages;
             const actors = res.data.objects.map(actor => actor);
             this.setState({actors});
-
+            const actorsGrouped = splitarray(actors, 3)
+            this.state.actorsGrouped = actorsGrouped;
+            console.log(this.state.actorsGrouped);
         });
     }
 
@@ -71,7 +85,7 @@ class Actors extends React.Component {
 
     renderDropdownButtonSortby(title, i) {
         return (
-            <DropdownButton bsStyle="primary" title={title} key={"name"} id={`dropdown-basic-${i}`}
+            <DropdownButton bsStyle="primary" title={title} key={"name"} id={'dropdown-basic-${i}'}
                             onSelect={this.handleSelectSort}>
                 <MenuItem eventKey="name">Name</MenuItem>
                 <MenuItem eventKey="birthday">Birthday</MenuItem>
@@ -96,7 +110,39 @@ class Actors extends React.Component {
 
             <div className="container" styles="margin-top:100px;">
                 <div className="row">
-                    <div className='text-center'>
+                  
+            <div className='text-center'>
+            {this.renderDropdownButtonSortby("Sort By: ", "name")}
+                        {this.renderDropdownButtonSortDirection("Order", "")}
+                </div>
+                </div>
+                <div className="row">
+                    {this.state.actorsGrouped[0]?
+                     this.state.actorsGrouped[0].map(actor =>
+                     
+                        <div className="col-sm-4">
+                            <div className="panel panel-info">
+                                <div className="panel-heading"><Link to={"/actor/" + actor.id}>{actor.name}</Link></div>
+                                <div className="panel-body"><img src={"https://image.tmdb.org/t/p/w640/" + actor.image}
+                                                                 className="img-responsive" style={imageStyles}
+                                                                 alt="Image"/></div>
+                            </div>
+                        </div>
+                    ):null}
+                    <div className="row">
+                    {this.state.actorsGrouped[1]?
+                     this.state.actorsGrouped[1].map(actor =>
+                     
+                        <div className="col-sm-4">
+                            <div className="panel panel-info">
+                                <div className="panel-heading"><Link to={"/actor/" + actor.id}>{actor.name}</Link></div>
+                                <div className="panel-body"><img src={"https://image.tmdb.org/t/p/w640/" + actor.image}
+                                                                 className="img-responsive" style={imageStyles}
+                                                                 alt="Image"/></div>
+                            </div>
+                        </div>
+                    ):null}
+                      <div className='text-center'>
 
                         {!this.state.numPages
                             ? null
@@ -113,22 +159,9 @@ class Actors extends React.Component {
                                 activePage={this.state.activePage}
                                 onSelect={this.handleSelect}/>
                         }
-
-                        {this.renderDropdownButtonSortby("Sort By: ", "name")}
-                        {this.renderDropdownButtonSortDirection("Order", "")}
                     </div>
-                </div>
-                <div className="row">
-                    {this.state.actors.map(actor =>
-                        <div className="col-sm-4">
-                            <div className="panel panel-info">
-                                <div className="panel-heading"><Link to={"/actor/" + actor.id}>{actor.name}</Link></div>
-                                <div className="panel-body"><img src={"https://image.tmdb.org/t/p/w640/" + actor.image}
-                                                                 className="img-responsive" style={imageStyles}
-                                                                 alt="Image"/></div>
-                            </div>
-                        </div>
-                    )}
+                        
+                    </div>
 
 
                 </div>
