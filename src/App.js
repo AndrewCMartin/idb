5686495199f1738529433eb99d6798efc8b61389
tesiawu <tesiawu@utexas.edu>
2017-11-09 13:54:22 -0600

    deleted commented out lines

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import ReactRouter from 'react-router-dom';
 
 import Header from './Header'
 import Main from './Main'
 import 'react-bootstrap';
-
-
-// var Router = ReactRouter.BrowserRouter;
-// var Route = ReactRouter.Route;
-// var Switch = ReactRouter.Switch;
-
 
 const App = () => (
   <div>
