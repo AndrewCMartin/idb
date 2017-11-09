d042568b98e4f7e40bf9a21f754dbe72623e195f
Hannah Anees <hannahanees@utexas.edu>
2017-11-09 04:24:10 +0000

    Updated styling to character instance pages

--- src/CharacterInstance.js
+++ src/CharacterInstance.js
@@ -17,7 +17,7 @@ var panelStyle = {
 }
 
 var headingStyle= {
-    color: 'white',
+    color:'#d3d1d1',
     borderColor: 'white',
     textTransform: 'uppercase',
 }
@@ -71,57 +71,64 @@ class CharacterInstance extends React.Component {
                     <div class="panel-body">
                         <div class="panel-body">
             <Row>
-            <Col xs={6} md ={6}>
+            <Col xs={5} md ={5}>
             <img src={this.state.character.thumbnail} class="img-responsive"
                                                      class="img-responsive" style={imageStyle} alt="Image"/>
             </Col>
-            <Col xs={6} md={6}>
+            <Col xs={7} md={7}>
             {/*  <h3>Information</h3>
                             <h3>Name</h3> 
                             <ul>
                                 <li>{this.state.character.name}</li>*/}
                                 <h3 style={headingStyle}>Description</h3>
                                 <p>{this.state.character.desc}</p>
+                                <br></br>
                                 <h3 style={headingStyle}>Stories</h3>
                                 <p>{this.state.character.stories}</p>
+                                <br></br>
              <h3 style={headingStyle}>Portrayed by Actors</h3>
                                     <ul>
                                         {actors.length > 0 ? actors.map(function (actor) {
 
 
                                             return (
-                                                <li key={actor.name}><Link to={'/actor/${actor.id}'} style={{color:'red', fontSize: '16px'}}>{actor.name}</Link>
-                                                </li>)
+                                                <p key={actor.name}><Link to={'/actor/${actor.id}'} style={{color:'#ed2f2f', fontSize: '17px'}}>{actor.name}</Link>
+                                                </p>)
                                         }) : "None"}
 
 
                                     </ul>
             </Col>
             </Row>
+            <hr></hr>
             <Row>
             <Col xs={3} md={3}>
-            <br></br>
+          
+            
             <br></br>
                                
-                                    <h4 style={secColStyle}>Events</h4>
+                                    <h3 style={secColStyle}>Events</h3>
+                                    <br></br>
                                     <ul>
                                         {events.length > 0 ? events.map(function (event) {
-                                            return (<li key={event.title}><Link
-                                                to={'/event/${event.id}'} style={{color:'red', fontSize: '16px'}}>{event.title}</Link></li>)
+                                            return (<li key={event.title} style={{color:'#d3d1d1'}}><Link
+                                                to={'/event/${event.id}'} style={{color:'#ed2f2f', fontSize: '17px'}}>{event.title}</Link></li>)
                                         }) : "None"}
                                     </ul>
                                 
             </Col>
             <Col xs={3} md={3}>
-            <br></br>
+            
+            
             <br></br>
 
                                 
-                                    <h4 style={secColStyle}>Comic Series</h4>
+                                    <h3 style={secColStyle}>Comic Series</h3>
+                                    <br></br>
                                     <ul>
                                         {series.length > 0 ? series.map(function (series_instance) {
-                                            return (<li key={series_instance.title}><Link
-                                                to={'/comic_series/${series_instance.id}'} style={{color:'red', fontSize: '16px'}}>{series_instance.title}</Link>
+                                            return (<li key={series_instance.title} style={{color:'#d3d1d1'}}><Link
+                                                to={'/comic_series/${series_instance.id}'} style={{color:'#ed2f2f', fontSize: '17px'}}>{series_instance.title}</Link>
                                             </li>)
                                         }) : "None"}
                                     </ul>
@@ -129,27 +136,31 @@ class CharacterInstance extends React.Component {
              </Col>
 
             <Col xs={3} md={3}>
-            <br></br>
+            
+            
             <br></br>
                                 
-                                    <h4 style={secColStyle}>Appears in Movies</h4>
+                                    <h3 style={secColStyle}>Appears in Movies</h3>
+                                    <br></br>
                                     <ul>
                                         {movies.length > 0 ? movies.map(function (movie) {
-                                            return (<li key={movie.title}><Link
-                                                to={'/movie/${movie.id}'} style={{color:'red', fontSize: '16px'}}>{movie.title}</Link></li>)
+                                            return (<li key={movie.title} style={{color:'#d3d1d1'}}><Link
+                                                to={'/movie/${movie.id}'} style={{color:'#ed2f2f', fontSize: '17px'}}>{movie.title}</Link></li>)
                                         }) : "None"}
                                     </ul>
                                 
                    </Col>
             <Col xs={3} md={3}>
-            <br></br>
+           
+            
             <br></br>
                                 
-                                    <h4 style={secColStyle}>Appears in TV Shows</h4>
+                                    <h3 style={secColStyle}>Appears in TV Shows</h3>
+                                    <br></br>
                                     <ul>
                                         {tvshows.length > 0 ? tvshows.map(function (tv_show) {
-                                            return (<li key={tv_show.name}><Link
-                                                to={'/tvshow/${tv_show.id}'} style={{color:'red', fontSize: '16px'}}>{tv_show.name}</Link></li>)
+                                            return (<li key={tv_show.name} style={{color:'#d3d1d1'}}><Link
+                                                to={'/tvshow/${tv_show.id}'} style={{color:'#ed2f2f', fontSize: '17px'}}>{tv_show.name}</Link></li>)
                                         }) : "None"}
                                     </ul>
                                
